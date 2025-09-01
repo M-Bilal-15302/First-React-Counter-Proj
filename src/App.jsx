bc6b9a3b8dd9484010101ff3e6dbe5d38fd03add
M-Bilal-15302 <bilalkhattak0318153@gmail.com>
2025-09-01 23:10:08 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+//--------------UseState-------
+
+function App() {
+
+  let [counter , setcounter] = useState(0)
+const addVal = ()=>{
+  setcounter(counter+1) 
+}
+const removeVal = ()=>{
+  setcounter(counter-1)
+}
+const resetVal = ()=>{
+  setcounter(counter = 0)
+}
+
+  return (
+    <>
+      <h1>BASIC COUNTER </h1>
+      <h3>counter value is : {counter} </h3>
+      <button onClick={addVal} id='add'
+      >Add value</button>
+      <button onClick={removeVal} id='remove'
+      >Remove value</button> <br /><br />
+      <button onClick={resetVal} id='reset'
+      >Reset value to 0</button>
+      
+          </>
+  )
+}
+
+export default App
